2451051527ee1009f75234b1dfe31cb5fbbfbd2e
JavDidier <javierre.didier@gmail.com>
2022-07-05 13:33:17 +0200

    Fonctionnel premier test

---
+++ main.js
@@ -0,0 +1,57 @@
+
+let recupererbarCode;
+let envoiCodeBarre = document.querySelector('button');
+
+envoiCodeBarre.addEventListener('click', function() {
+    recupererbarCode = document.querySelector('#monId').value;
+    appel(recupererbarCode);
+});
+
+
+/* APPEL A L API */
+function appel(barCode) {
+    let url = "https://world.openfoodfacts.org/api/v0/product/" + barCode;
+
+    /* CRÉER UNE REQUETE */
+    let requete = new XMLHttpRequest();
+    requete.open("GET", url );
+    requete.responseType = 'json';
+    requete.send();
+
+    /* QUAND ON A LA REPONSE */
+    requete.onload = function() {
+        if(requete.readyState === XMLHttpRequest.DONE) {
+            if(requete.status === 200) {
+                let reponse = requete.response; // récupérer la requette complète
+                let resultatCode = reponse.code; // récupérer code barre
+                let resultatTitle = reponse.product.generic_name_fr; // récupérer code barre
+
+                let resultat = reponse.product.ingredients_text_fr;
+                let resultatImgLink = reponse.product.image_front_thumb_url;
+
+                document.querySelector('.resultat').textContent = resultat;
+                document.querySelector(".resultatCode").textContent = resultatCode;
+                document.querySelector(".resultatTitre").textContent = resultatTitle;
+                document.querySelector(".resultatImg").src= resultatImgLink;
+           }
+            else {
+                alert("Erreur de fichier ...");
+            }
+        }
+        else {
+            console.log("Erreur");
+        }
+    }
+}
+
+function check() {
+    barCode = document.querySelector('#monId').value;
+    console.log(barCode);
+}
+
+
+
+
+
+
+
